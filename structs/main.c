ced1a11dcb9961f9fe1202a2206b02123b1acba2
Taz Uddin <getleton@gmail.com>
2025-11-16 02:47:20 +0600

    Use typedef

--- structs/main.c
+++ structs/main.c
@@ -1,23 +1,29 @@
 
 
-#include "person.h"
-#include "point.h"
+#include "typedef_person.h"
+#include "typedef_point.h"
 #include <stdio.h>
 #include <stdlib.h>
 
 int main(int argc, char *argv[]) {
 
+  printf("size of int: %zu\n", sizeof(int));
+  printf("size of char: %zu\n", sizeof(char));
+  printf("size of char*: %zu\n", sizeof(char *));
+  printf("size of Point*: %zu\n", sizeof(Point));
+  printf("size of Person*: %zu\n", sizeof(Person));
+
   printf("finally learning what c structs are about\n");
 
-  struct Point p;
+  Point p;
   p.X = 10;
   p.Y = 10;
   p.Z = 11;
 
-  struct Point p2 = {0};
-  struct Point p3 = {10, 10, 3};
-  struct Person person = {0};
-  struct Person person2 = {.last_name = "Taz", .age = 35};
+  Point p2 = {0};
+  Point p3 = {10, 10, 3};
+  Person person = {0};
+  Person person2 = {.last_name = "Taz", .age = 35};
 
   // print_point(p);
   point_json(p);
@@ -29,16 +35,19 @@ int main(int argc, char *argv[]) {
   return EXIT_SUCCESS;
 }
 
-void print_point(struct Point p) {
+void print_point(Point p) {
   printf("The point p: x = %d, y = %d, z = %d\n", p.X, p.Y, p.Z);
 }
 
-void point_json(struct Point p) {
+void point_json(Point p) {
+  printf("size of Point: %lu\n", sizeof(Point));
+
   printf("{\n\t\"X\":%d,\n\t\"Y\":%d,\n\t\"Z\":%d\n\b}", p.X, p.Y, p.Z);
   printf("\n");
 }
 
-void person_json(struct Person p) {
+void person_json(Person p) {
+  printf("size of Person: %zu\n", sizeof(Person));
   printf("{\n\t\"firstName\":%s,\n\t\"lastName\":%s,\n\t\"age\":%d\n\b}",
          p.first_name, p.last_name, p.age);
   printf("\n");
