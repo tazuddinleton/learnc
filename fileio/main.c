7e853ccfd135b14c651a4d3a057eee905ae1d35b
Taz Uddin <getleton@gmail.com>
2025-12-01 23:19:47 +0600

    Add a simple command line flag parser

--- fileio/main.c
+++ fileio/main.c
@@ -3,12 +3,72 @@
 #include "csv_parser.h"
 #include "logger.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct cmd_args {
+	char *file_path;
+	LOG_LEVEL loglevel;
+} CmdArgs;
+
+void print_usage() {
+
+	printf("Usage: csvp <filepath> [-l loglevel]\n");
+	printf("Options: \n");
+	printf("	-l <level>	Set log level i.e. trace, debug, info, warn, "
+		   "error, fatal");
+}
+
+/*Todo: Implement a robust cmd flag parser*/
+CmdArgs *parse_cmd(int argc, char *argv[]) {
+	if (argc < 2) {
+		print_usage();
+		return NULL;
+	}
+
+	if (strcmp(argv[2], "-l") == 0 && argc != 4) {
+		printf("No argument was provided to -l\n\n");
+		print_usage();
+		return NULL;
+	}
+
+	CmdArgs *cmd_args = malloc(sizeof(CmdArgs));
+	cmd_args->file_path = argv[1];
+
+	if (argc == 4 && strcmp(argv[2], "-l") == 0) {
+
+		char *flag = argv[3];
+
+		if (strcmp(flag, "trace") == 0) {
+			cmd_args->loglevel = LOG_LEVEL_TRACE;
+		} else if (strcmp(flag, "debug") == 0) {
+			cmd_args->loglevel = LOG_LEVEL_DEBUG;
+		} else if (strcmp(flag, "info") == 0) {
+			cmd_args->loglevel = LOG_LEVEL_INFO;
+		} else if (strcmp(flag, "warn") == 0) {
+			cmd_args->loglevel = LOG_LEVEL_WARNING;
+		} else if (strcmp(flag, "error") == 0) {
+			cmd_args->loglevel = LOG_LEVEL_ERROR;
+		} else if (strcmp(flag, "error") == 0) {
+			cmd_args->loglevel = LOG_LEVEL_FATAL;
+		} else {
+			printf("Unknown argument: %s\n\n", flag);
+			print_usage();
+			return NULL;
+		}
+	}
+	return cmd_args;
+}
+
 int main(int argc, char *argv[]) {
 
-	set_current_log_level(LOG_LEVEL_INFO);
-	log_info("trying to read the data.csv\n");
+	CmdArgs *cmd_args = parse_cmd(argc, argv);
+	if (cmd_args == NULL) {
+		return 1;
+	}
 
-	ParseResult *result = parse_csv("data.csv");
+	set_current_log_level(LOG_LEVEL_ERROR);
+	ParseResult *result = parse_csv(cmd_args->file_path);
 
 	for (int i = 0; i < result->length; i++) {
 		printf("%s\n", result->data[i]);
