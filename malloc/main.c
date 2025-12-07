6edb627c9ca1a51ec8fc3380deec4d04d9e58f99
Taz Uddin <getleton@gmail.com>
2025-12-07 22:48:31 +0600

    Keep practicing malloc

--- malloc/main.c
+++ malloc/main.c
@@ -9,7 +9,28 @@ char *get_full_greeting(char *greeting, char *name, int size) {
 	return full_greeting;
 }
 
+int *initialize_scalar_list(int size, int multiplier) {
+	int *result = malloc(size * sizeof(int));
+	if (result == NULL) {
+		return NULL;
+	}
+
+	for (int i = 0; i < size; i++) {
+		result[i] = i * multiplier;
+	}
+	return result;
+}
+
 int main() {
 	printf("greeting: %s\n", get_full_greeting("Hello,", "Taz", 12));
+
+	printf("size of array: 10\n");
+	int scalar_size = 10;
+	int multiplier = 2;
+	int *scalar_list = initialize_scalar_list(scalar_size, multiplier);
+	for (int i = 0; i < scalar_size; i++) {
+		printf("scalar item: %d => %d\n", i, scalar_list[i]);
+	}
+
 	return 0;
 }
